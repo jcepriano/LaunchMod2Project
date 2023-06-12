2d8047167fd380d7f1ea93bc03a8ff8a92c2bffe
jcepriano <jamescepriano@gmail.com>
2023-06-12 09:53:58 -0700

    Annotated on how to complete iteration 2

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -34,6 +34,9 @@ while (userInput.ToLower() != "quit")
 
         userInput = Console.ReadLine();
         Console.WriteLine();
+        
+        // Create instance of message class and add user input
+        // Add messages to the message context and save changes
     }
 
     Console.Write("Would you like to log in a `new` or `existing` user? Or, `quit`? ");
@@ -49,6 +52,8 @@ while (userInput.ToLower() != "quit")
         Console.Write("Add a message: ");
 
         userInput = Console.ReadLine();
+        // Create instance of user class and add the user input to the user
+        // Add the room to the room context and save changes
 
     }
     else if (userInput.ToLower() == "existing")
